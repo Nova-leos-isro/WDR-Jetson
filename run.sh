b3410e145152b235ea2e775ef3c89d73083fa74d
NovaLeos <nithish.isro.leos@gmail.com>
2025-09-02 15:09:56 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+pip install gdown
+
+
+
 gdown --fuzzy https://drive.google.com/file/d/1nYKpWzuQCa-ck-DUSUVLQ8-7tZjLGAol/view?usp=drive_link
 
 gdown --fuzzy https://drive.google.com/file/d/1ScnKWqGgx_j7cYNYgJZp3SltM_pRxx_b/view?usp=drive_link
