3d1fa7b112b83150981d885dbbd5acbe3a8a429a
NovaLeos <nithish.isro.leos@gmail.com>
2025-09-02 06:48:46 +0000

    added shell script

---
+++ run.sh
@@ -0,0 +1,13 @@
+gdown --fuzzy https://drive.google.com/file/d/1nYKpWzuQCa-ck-DUSUVLQ8-7tZjLGAol/view?usp=drive_link
+
+gdown --fuzzy https://drive.google.com/file/d/1ScnKWqGgx_j7cYNYgJZp3SltM_pRxx_b/view?usp=drive_link
+
+gdown --fuzzy https://drive.google.com/file/d/19-wbsK7D0J0-9lvcj8d68_y6Pgod9kHH/view?usp=sharing
+
+unzip archive.zip
+
+
+unzip 'archive (1).zip'
+
+
+unzip file.zip
